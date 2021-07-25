2baced7a32750617066d512169beb2961a5fbaf2
cbossie <cbossie@gmail.com>
2021-07-25 15:12:57 -0400

    Updated so everything works

---
+++ ContainerConsoleApp/Code/SimulationWorker.cs
@@ -0,0 +1,131 @@
+﻿using Amazon.S3;
+using Amazon.S3.Model;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ContainerConsoleApp.Code
+{
+    public class SimulationWorker : IHostedService
+    {
+        IAmazonS3 S3Cli { get; init; }
+        SimulationConfig SimConfig { get; init; }
+        IHostApplicationLifetime Lifetime { get; init; }
+        Random Rnd = new Random();
+        ILogger Logger { get; init; }
+
+        public SimulationWorker(IAmazonS3 s3Cli, SimulationConfig simConfig, IHostApplicationLifetime lifetime, ILogger<SimulationWorker> logger)
+        {
+            S3Cli = s3Cli;
+            SimConfig = simConfig;
+            Lifetime = lifetime;
+            Logger = logger;
+        }
+
+        private void Complete()
+        {
+            Lifetime.StopApplication();
+        }
+
+        private (int, TimeSpan) RunSimulation(int repetitions)
+        {
+            var watch = new System.Diagnostics.Stopwatch();
+            watch.Start();
+            int tries = 0;
+            List<int> uniqueValues = new();
+            bool found = default;
+            int numresults = 0;
+            
+
+            while (numresults < repetitions)
+            {
+                found = false;
+                tries++;
+                int nextResult = Rnd.Next(repetitions);
+
+                foreach(var n in uniqueValues)
+                {
+                    if(nextResult == n)
+                    {
+                        found = true;
+                    }
+                }
+
+                if (!found)
+                {
+                    uniqueValues.Add(nextResult);
+                    numresults++;
+                    Logger.LogInformation($"Adding unique value {nextResult}");
+                }
+                else
+                {
+                    Logger.LogInformation($"Value {nextResult} is already in the list.");
+                }
+
+                Logger.LogInformation($"Try # {tries}. Array contains {uniqueValues.Count} / {repetitions} values");
+
+                if (tries == int.MaxValue)
+                {
+                    Logger.LogInformation($"Could not find {repetitions} unique values before int32 overflowed");
+                    break;
+                }
+
+            }
+            watch.Stop();
+    
+            return (tries, watch.Elapsed);
+        }
+
+        private static string UniqueKey => DateTime.Now.ToString("s");
+
+        private async Task WriteStringToBucket(string message)
+        {
+            try
+            {
+                await S3Cli.PutObjectAsync(new PutObjectRequest
+                {
+                    BucketName = SimConfig.Bucket,
+                    ContentBody = message,
+                    Key = UniqueKey
+
+
+                }).ConfigureAwait(false);
+
+            }
+            catch(InvalidOperationException ex)
+            {
+                Logger.LogError(ex, ex.Message);
+            }
+        }
+
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            try
+            {
+                var (tries, span) = RunSimulation(SimConfig.Repetitions);
+                StringBuilder sb = new StringBuilder();
+
+                sb.AppendLine($"Generated {SimConfig.Repetitions} unique values in {tries} tries");
+                sb.AppendLine($"It took {span.TotalMinutes} minutes");
+                await WriteStringToBucket(sb.ToString());
+                Logger.LogInformation(sb.ToString());
+                Complete();
+
+            }
+            finally
+            {
+                Complete();
+            }
+        }
+
+        public async Task StopAsync(CancellationToken cancellationToken)
+        {
+            Logger.LogInformation("Completed");
+        }
+    }
+}
--- ContainerConsoleApp/Program.cs
+++ ContainerConsoleApp/Program.cs
@@ -1,12 +1,62 @@
-﻿using System;
+﻿using Amazon.S3;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Amazon.Extensions;
+using ContainerConsoleApp.Code;
+using Microsoft.Extensions.Logging;
 
 namespace ContainerConsoleApp
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //IConfiguration configuration = CreateConfiguration(args);
+            using IHost host = CreateHostBuilder(args/*, configuration*/).Build(); ;
+
+
+            Console.WriteLine("Starting!");
+
+            await host.RunAsync();
+
+            Console.WriteLine("Done");
+
         }
+
+
+        static IHostBuilder CreateHostBuilder(string[] args/*, IConfiguration configuration*/)
+        {
+            var host = Host.CreateDefaultBuilder()
+                .ConfigureLogging(logging =>
+                {
+                    logging.ClearProviders();
+                    logging.AddConsole();
+
+                })
+                .ConfigureServices((context, services) =>
+                {
+                    var config = context.Configuration;
+                    var options = config.GetAWSOptions();
+                    services.AddAWSService<IAmazonS3>(options);
+
+
+                    var bucket = config["BUCKET"];
+                    int repetitions = int.Parse(config["REPETITIONS"]);
+                    SimulationConfig simConf = new SimulationConfig(bucket, repetitions);
+                    services.AddSingleton(simConf);
+
+                    services.AddLogging(a => a.AddConsole());
+
+                    services.AddHostedService<SimulationWorker>();
+
+
+                });
+            return host;
+        }
+
+
     }
 }
